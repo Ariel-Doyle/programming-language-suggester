f42cb5b8203271ae1a3c9be0dbec41fe49c4c938
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-05 16:08:57 -0700

    perform some debugging using console.log to check if code is running.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 //UI Logic
+window.addEventListener("load", function() {
 
-function getAndSetRadioButtonValues() {
-  e.preventDefault();
-  const radioSelection1 = document.querySelectorAll("input[name='decide1']:checked").value;
-  const radioSelection2 = document.querySelectorAll("input[name='decide2']:checked").value;
-  const radioSelection3 = document.querySelectorAll("input[name='decide3']:checked").value;
-  const radioSelection4 = document.querySelectorAll("input[name='decide4']:checked").value;
-  const radioSelection5 = document.querySelectorAll("input[name='decide5']:checked").value;
- 
-  let result;
-  if (radioSelection1 === "Agree") {
-    result = "C#";
-  } else if (radioSelection1 === "Disagree") {
-    result = "Ruby";
-  } else if (radioSelection1 === "Neutral") {
-    result = "Python";
+  function getAndSetRadioButtonValues() {
+    const radioSelection1 = document.querySelector("input[name='decide1']:checked").value;
+    const radioSelection2 = document.querySelector("input[name='decide2']:checked").value;
+    const radioSelection3 = document.querySelector("input[name='decide3']:checked").value;
+    const radioSelection4 = document.querySelector("input[name='decide4']:checked").value;
+    const radioSelection5 = document.querySelector("input[name='decide5']:checked").value;
+    
+    let result;
+    if (radioSelection1 === "Agree") {
+      result = "C#";
+    } else if (radioSelection1 === "Disagree") {
+      result = "Ruby";
+    } else if (radioSelection1 === "Neutral") {
+      result = "Python";
+    }
+
+    document.getElementById("output").innerText = result;
   }
-}
 
-function setFormSubmissionEventHandler() {
-  let form = document.querySelector("form");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-  });
-}
+  function setFormSubmissionEventHandler() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      getAndSetRadioButtonValues();
+    });
+  }
+
+  setFormSubmissionEventHandler();
+});
