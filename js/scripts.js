9b5d336db17b2fbae82854e3cf5072f4d4cb157c
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-05 15:21:31 -0700

    WIP, research JavaScript for radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 //UI Logic
 
-window.addEventListener("load", function() {
+function getAndSetRadioButtonValues() {
+  e.preventDefault();
   const radioSelection1 = document.querySelectorAll("input[name='decide1']:checked").value;
   const radioSelection2 = document.querySelectorAll("input[name='decide2']:checked").value;
   const radioSelection3 = document.querySelectorAll("input[name='decide3']:checked").value;
   const radioSelection4 = document.querySelectorAll("input[name='decide4']:checked").value;
   const radioSelection5 = document.querySelectorAll("input[name='decide5']:checked").value;
-
-
-  function setFormSubmissionEventHandler() {
-    let form = document.querySelector("form");
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-    });
+ 
+  let result;
+  if (radioSelection1 === "Agree") {
+    result = "C#";
+  } else if (radioSelection1 === "Disagree") {
+    result = "Ruby";
+  } else if (radioSelection1 === "Neutral") {
+    result = "Python";
   }
-});
+}
+
+function setFormSubmissionEventHandler() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+  });
+}
