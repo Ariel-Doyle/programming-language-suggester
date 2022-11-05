d016db40bcfb38ae4f741150f27ff2fc2f47e73d
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-05 12:24:24 -0700

    add initial HTML for radio button groups agree, disagree, and neutral for each of 5 questions.

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+//UI Logic
+
+window.addEventListener("load", function() {
+  function handleRadio(e) {
+    e.preventDefault();
+    const radioSelection = document.querySelector("input[name='decide']:checked").value;
+  }
+
+  /*window.addEventListener("load", function() {
+    document.getElementById("radio-form").addEventListener("submit", handleRadio);*/
+
+});
+
+
+
+/*function getInitialWhisperValue() {
+  const personInput = document.getElementById("personInput").value;
+  const whisper = document.querySelector("span#personInput1");
+  whisper.innerText = personInput.toLowerCase();
+}
+
+function returnSmallerFont() {
+  getInitialWhisperValue();
+    const computedFont = window.getComputedStyle(whisper, null);
+    let computedSize = computedFont.getPropertyValue('font-size');
+    let smallerFont = parseFloat(computedSize) * .9;
+    whisper.style.fontSize = smallerFont + 'px';
+}
+ 
+function setFormSubmissionEventHandler() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    returnSmallerFont();
+    document.querySelector("div#whisper").removeAttribute("class");
+  });
+}
+
+window.addEventListener("load", function () {
+  setFormSubmissionEventHandler();
+});*/
