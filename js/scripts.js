72051094a3b1030b64ffefef6946a69e3199b894
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-05 14:57:05 -0700

    define variables for radio buttons and assign values of checked button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,17 @@
 //UI Logic
 
 window.addEventListener("load", function() {
-  function getAndSetQuestionValues() {
-    const question1Input = document.getElementById()
+  const radioSelection1 = document.querySelectorAll("input[name='decide1']:checked").value;
+  const radioSelection2 = document.querySelectorAll("input[name='decide2']:checked").value;
+  const radioSelection3 = document.querySelectorAll("input[name='decide3']:checked").value;
+  const radioSelection4 = document.querySelectorAll("input[name='decide4']:checked").value;
+  const radioSelection5 = document.querySelectorAll("input[name='decide5']:checked").value;
+
+
+  function setFormSubmissionEventHandler() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+    });
   }
-  
-  const form = document.querySelector("form");
-
-  
-  
-  function handleRadio(e) {
-    const radioSelection = document.querySelector("input[name='decide']:checked").value;
-  }
-
-  /*window.addEventListener("load", function() {
-    document.getElementById("radio-form").addEventListener("submit", handleRadio);*/
-
 });
-
-/*function getInitialWhisperValue() {
-  const personInput = document.getElementById("personInput").value;
-  const whisper = document.querySelector("span#personInput1");
-  whisper.innerText = personInput.toLowerCase();
-}
-
-function returnSmallerFont() {
-  getInitialWhisperValue();
-    const computedFont = window.getComputedStyle(whisper, null);
-    let computedSize = computedFont.getPropertyValue('font-size');
-    let smallerFont = parseFloat(computedSize) * .9;
-    whisper.style.fontSize = smallerFont + 'px';
-}
- 
-function setFormSubmissionEventHandler() {
-  let form = document.querySelector("form");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    returnSmallerFont();
-    document.querySelector("div#whisper").removeAttribute("class");
-  });
-}
-
-window.addEventListener("load", function () {
-  setFormSubmissionEventHandler();
-});*/
